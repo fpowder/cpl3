8be8d0da900faa88e1e44ca92dfccd7ce38bd04b
joohyun jo <jjh@alt-a.net>
2022-12-05 18:39:59 +0900

    feature: fix parking area scale for border lines

--- src/component/ParkingArea.ts
+++ src/component/ParkingArea.ts
@@ -1,3 +1,4 @@
+import { timingSafeEqual } from 'crypto';
 import { BoxGeometry, EdgesGeometry, LineBasicMaterial, LineSegments, Mesh, MeshPhongMaterial, Scene } from 'three';
 import parkingAreaCords from '../config/parkingAreaCords';
 import { cpl3Scene } from '../module/Basic';
@@ -12,8 +13,12 @@ export default class ParkingArea {
 
     constructor(cpl3Scene: Scene, cord: {start: number[], vector: number[]}) {
 
-        this.geometry = new BoxGeometry(cord.vector[0], 0.05, cord.vector[1]);
+        this.geometry = new BoxGeometry(cord.vector[0], 0, cord.vector[1]);
+        this.geometry.scale(0.98, 1, 0.98);
         this.material = new MeshPhongMaterial({color: '#423e80'});
+        this.material.polygonOffset = true;
+        this.material.polygonOffsetFactor = -3;
+        this.material.polygonOffsetUnits = 0.1;
 
         this.mesh = new Mesh(this.geometry, this.material);
         this.mesh.position.x = cord.start[0] + cord.vector[0] / 2 /* + settings.spacer / 2 */;
@@ -22,8 +27,15 @@ export default class ParkingArea {
 
         // parking area border
         const borderGeo = new EdgesGeometry(this.geometry);
-        const borderMat = new LineBasicMaterial({ color: 0xffffff, linewidth: 2 });
+        const borderMat = new LineBasicMaterial({ color: 0xffffff, linewidth: 3 });
+        borderMat.polygonOffset = true;
+        borderMat.polygonOffsetFactor = -3;
+        borderMat.polygonOffsetUnits = 0.1;
+        
         const wireframe = new LineSegments(borderGeo, borderMat);
+        wireframe.material.polygonOffset = true;
+        wireframe.material.polygonOffsetFactor = -3;
+        wireframe.material.polygonOffsetUnits = 0.1;
 
         wireframe.position.x = this.mesh.position.x;
         wireframe.position.y = this.mesh.position.y;
