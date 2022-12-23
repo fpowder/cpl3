b9954da0f64f9b3001d08227542b1a639f212995
joohyun jo <jjh@alt-a.net>
2022-12-23 19:59:58 +0900

    feature: raycaster for interacts working on

--- src/component/Car.ts
+++ src/component/Car.ts
@@ -1,5 +1,5 @@
-import { gltfLoader, gsap } from '../module/Basic';
-import { MathUtils, AnimationMixer, Mesh, Scene, AnimationAction, BoxGeometry, MeshLambertMaterial, QuadraticBezierCurve3, Vector3, Vector} from 'three';
+import { cpl3Scene, gltfLoader, gsap, raycaster } from '../module/Basic';
+import { MathUtils, AnimationMixer, Mesh, Scene, AnimationAction, BoxGeometry, MeshLambertMaterial, QuadraticBezierCurve3, Vector3, Vector, LineBasicMaterial, Line, BufferGeometry, Object3D} from 'three';
 import carGlb from '../asset/resource/models/car.glb';
 
 // ParkingArea move path import
@@ -8,7 +8,7 @@ import path from '../config/path';
 import { vec3fromObj, drawBezierPath } from '../module/Util';
 export default class Car {
 
-    mesh: Mesh;
+    mesh: any;
     mixer: AnimationMixer;
 
     moving: boolean;
@@ -17,7 +17,7 @@ export default class Car {
     backwardAction: AnimationAction;
 
     stdDistance: number = 4;
-    stdSpeed: number = 20;
+    stdSpeed: number;
 
     bezierPoints: number = 30;
     startZOffset: number = 20;
@@ -28,8 +28,12 @@ export default class Car {
     parked: boolean = false;
     wayout: boolean = false;
 
-    constructor(cpl3Scene: Scene) {
-        
+    frontSensor: Line;
+    frontSensorHeight: number = 2;
+    frontSensorLength: number = 7;
+
+    constructor(cpl3Scene: Scene, stdSpeed: number) {
+        this.stdSpeed = stdSpeed;
         gltfLoader.load(
             carGlb,
             (gltf) => {
@@ -40,10 +44,11 @@ export default class Car {
                         child.castShadow = true;
                     }
                 });
-                console.log(gltf.scene.children[0]);
-                console.log(gltf.animations);
-                this.mesh = gltf.scene.children[0] as Mesh;
-
+                console.log('gltf.scene.children : ',gltf.scene.children[0]);
+                // console.log(gltf.animations);
+                this.mesh = gltf.scene.children[0];
+                this.mesh.name = 'car';
+                // console.log('this.mesh', this.mesh);
                 // entrance position
                 this.mesh.position.set(path[0].x, path[0].y, path[0].z - this.startZOffset);
                 
@@ -62,6 +67,18 @@ export default class Car {
                 this.backwardAction = this.mixer.clipAction(gltf.animations[0]);
                 this.backwardAction.clampWhenFinished = true;
 
+                // set frontSensor
+                const lineMat = new LineBasicMaterial({ color: 'yellow' });
+                const points = [
+                    new Vector3(0, this.frontSensorHeight, 0),
+                    new Vector3(0, this.frontSensorHeight, this.frontSensorLength)
+                ];
+
+                const lineGeo = new BufferGeometry().setFromPoints(points);
+                this.frontSensor = new Line(lineGeo, lineMat);
+                this.frontSensor.position.set(path[0].x, path[0].y, path[0].z - this.startZOffset);
+                cpl3Scene.add(this.frontSensor);
+
                 // entrance animation start
                 this.moveEntrancePath();
 
@@ -75,7 +92,8 @@ export default class Car {
 
         // 주차장 진입 애니매이션 설정
         entranceTl.to(
-            this.mesh.position,
+            // this.mesh.position,
+            [this.mesh.position, this.frontSensor.position],
             {
                 ease: 'none',
                 x: path[0].x,
@@ -87,16 +105,20 @@ export default class Car {
 
                     const finalDuration = basicDuration * ( dist / this.stdDistance );
                     return finalDuration;
-                })()
+                })(),
+                onUpdate: (() => {
+                    this.sensorRay(new Vector3(path[0].x, path[0].y, path[0].z));
+                })
             }
         ); // entranceTl.to
 
         const quadraticPath = path[1].quadraticPath;
-        this.bezierPath(entranceTl, quadraticPath, () => {
+        this.bezierPath(entranceTl, quadraticPath, [this.mesh.position, this.frontSensor.position], () => {
             /**
              *  entrance 애니메이션 종료 후, 주차장 경로를 따라 이동하는 함수 호출
              *  */
             this.mesh.lookAt(new Vector3(47.5, 0, 121));
+            this.frontSensor.lookAt(new Vector3(47.5, 0, 121));
             this.movePath();
         });
 
@@ -114,11 +136,11 @@ export default class Car {
             const eachPath = path[i];
 
             if(eachPath.quadraticPath) {
-                this.bezierPath(movePathTl, eachPath.quadraticPath);
+                this.bezierPath(movePathTl, eachPath.quadraticPath, [this.mesh.position, this.frontSensor.position]);
 
             } else { 
                 movePathTl.to(
-                    this.mesh.position,
+                    [this.mesh.position, this.frontSensor.position],
                     {
                         ease: 'none',
                         x: eachPath.x,
@@ -133,7 +155,9 @@ export default class Car {
                         })(),
                         onUpdate: () => {
                             // console.log(this.mesh.position);
-                            this.mesh.lookAt(new Vector3(path[i].x, path[i].y, path[i].z));
+                            const nextPath = new Vector3(path[i].x, path[i].y, path[i].z);
+                            this.mesh.lookAt(nextPath);
+                            this.sensorRay(nextPath);
                         }
                     }
                 );
@@ -143,8 +167,9 @@ export default class Car {
 
     bezierPath(
         timeline: gsap.core.Timeline, 
-        quadraticPath: {x: number, y: number, z: number}[], 
-        onComplete?: gsap.Callback
+        quadraticPath: {x: number, y: number, z: number}[],
+        positions: Vector3[],
+        onComplete?: gsap.Callback,
     ): void {
 
         const quadrarticBezier = new QuadraticBezierCurve3(
@@ -162,7 +187,7 @@ export default class Car {
             if(i !== points.length - 2){
                 
                 timeline.to(
-                    this.mesh.position,
+                    positions,
                     {
                         ease: 'none',
                         x: points[i].x,
@@ -177,13 +202,14 @@ export default class Car {
                         })(),
                         onUpdate: () => {
                             this.mesh.lookAt(points[i]);
+                            this.sensorRay(points[i]);
                         }
                     }
                 );
 
             } else {
                 timeline.to(
-                    this.mesh.position,
+                    positions,
                     {
                         ease: 'none',
                         x: points[i].x,
@@ -200,12 +226,33 @@ export default class Car {
                         onComplete: onComplete,
                         onUpdate: () => {
                             this.mesh.lookAt(points[i]);
+                            this.sensorRay(points[i]);
                         }
                     }
                 );
             } // if else
         }
 
+    }
+
+    sensorRay(direction: Vector3) {
+
+        this.frontSensor.lookAt(direction);
+        // console.log('cpl3Scene.children', cpl3Scene.children);
+        const pos: Vector3 = this.mesh.position;
+        const origin: Vector3 = new Vector3(pos.x, pos.y + this.frontSensorHeight, pos.z);
+        const rayDirect: Vector3 = new Vector3(direction.x, direction.y + this.frontSensorHeight, direction.z);
+
+        raycaster.set(origin, rayDirect);
+        const intersects = raycaster.intersectObjects(cpl3Scene.children);
+        // console.log('intersects', intersects);
+        for(const item of intersects) {
+            // console.log(item); 
+            // console.log('item type', item.object.type);
+            // console.log('item name', item.object.name);
+
+            if(item.object.name === 'car') console.log('car ray ', true);
+        }
     }
 
 }
